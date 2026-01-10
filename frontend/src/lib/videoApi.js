ce30bb7ef4cf001f8cf79ba72b13123aa01147bf
touhid404 <touhidulislamriyadh@gmail.com>
2026-01-10 23:56:45 +0600

    api issues fixed

--- frontend/src/lib/videoApi.js
+++ frontend/src/lib/videoApi.js
@@ -5,17 +5,17 @@ export const videoApi = {
         if (!userId) {
             throw new Error("User ID is required for video token");
         }
-        
-        const response = await axiosInstance.post("/video/token", {
+
+        const response = await axiosInstance.post("/workspaces/video/token", {
             userId,
             userName: userName || "Guest",
         });
-        
+
         // Verify response has required fields
         if (!response.data?.token) {
             throw new Error("Invalid token response from server");
         }
-        
+
         return response.data;
     },
 };
